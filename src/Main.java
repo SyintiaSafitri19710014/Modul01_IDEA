0e6db876b57934d2ff193dc7f9c0457bb566c13d
Syintia <syintiasftri@gmail.com>
2021-06-12 12:58:06 +0700

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Syintia";
+        String namaBelakang = "Safitri";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.45;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaBelakang);
+    }
+}
+
+
+
+
