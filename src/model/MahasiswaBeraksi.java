6737803743911c079d0673d6487aefd3ddf2370f
Syintia <syintiasftri@gmail.com>
2021-06-15 13:44:50 +0700

    Enkapsulasi

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -4,28 +4,49 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
+    public MahasiswaBeraksi() {
+    }
+
+    public static void main(String[] args) throws ParseException{
 
         String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateformat =new  SimpleDateFormat(polaTanggal);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19710014";
-        mahasiswa.nama = "Syintia";
-        mahasiswa.tanggalLahir =  simpleDateformat.parse("01-01-2001");
+        mahasiswa.nama = "Syintia Safitri";
+        mahasiswa.tanggalLahir = simpleDateFormat.parse( "01-01-2002");
 
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+        System.out.println("Npm \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa.hitungUsia() + " Tahun");
+        System.out.println("");
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
-        mahasiswa2.npm = "19710028";
-        mahasiswa2.nama = "Farina";
-        mahasiswa2.tanggalLahir = simpleDateformat.parse("11-03-2001");
+        mahasiswa2.npm = "19710083";
+        mahasiswa2.nama = "Febri";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse( "05-03-2001");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+        System.out.println("Npm \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa2.hitungUsia() + " Tahun");
+        System.out.println("");
+
+        Mahasiswa mahasiswa3 = new Mahasiswa();
+        mahasiswa3.npm = "19710089";
+        mahasiswa3.nama = "Riski Nurliana";
+        mahasiswa3.tanggalLahir = simpleDateFormat.parse( "28-03-2001");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa3.nama);
+        System.out.println("Npm \t\t\t : " + mahasiswa3.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa3.tanggalLahir));
+        System.out.println("Usia : \t\t\t : " + mahasiswa3.hitungUsia() + " Tahun");
+        System.out.println("");
+
+        mahasiswa.menyapa();
     }
 }
